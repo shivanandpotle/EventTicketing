a8b6d78cd71197e6f2293a6960c240de8eb13904
Shivanandpotle <shivanandpotle41@gmail.com>
2025-08-19 20:13:11 +0530

    Update server.js
    made changes in port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { connectToDatabase, getDb } = require('./database');
 const { sendTicketEmail } = require('./email');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
