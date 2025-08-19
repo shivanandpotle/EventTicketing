9bf311819097eef7b9a6f3fbc3f77386f35e2a58
Shivanandpotle <shivanandpotle41@gmail.com>
2025-08-19 20:12:14 +0530

    Update server.js
    added ports

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const { connectToDatabase, getDb } = require('./database');
 const { sendTicketEmail } = require('./email');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // --- Razorpay Instance ---
 const razorpay = new Razorpay({
