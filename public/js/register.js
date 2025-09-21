f81a3c2a471589f525c589be014dda477a840ca4
Shivanandpotle <shivanandpotle41@gmail.com>
2025-09-21 14:48:15 +0530

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -171,7 +171,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (!orderResult.success) throw new Error(orderResult.message);
 
                 const rzp = new Razorpay({
-                    key: 'rzp_live_RJ7y0j6O5Aa9vj', // This should ideally be fetched from the server
+                    key: 'rzp_test_RKCIdtuFRLjxQr', // This should ideally be fetched from the server
                     amount: orderResult.order.amount,
                     currency: 'INR',
                     name: 'Sambhav Club',
